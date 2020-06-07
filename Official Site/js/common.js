c207fa5666693ffdd63b17de8c21391977803ef7
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-07 22:45:05 +0800

    服务器迁移

--- Official Site/js/common.js
+++ Official Site/js/common.js
@@ -1,5 +1,5 @@
 var SERVER_URL = 'http://106.75.216.135:8004'
-var Resource_URL = 'http://app.kangfupanda.com/site/'
+var Resource_URL = 'http://47.111.166.154:8004/'
 var $header = `<!-- !Top-bar -->
 <div id="top-bar" role="complementary" class="bit-top-bar"
     style="line-height:30px;padding-right:0px;padding-left:0px;">
