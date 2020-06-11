6a3231718c6944fab5be013688f2c29de0cfaca2
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-12 00:27:29 +0800

    web api 使用域名

--- Official Site/js/common.js
+++ Official Site/js/common.js
@@ -1,5 +1,5 @@
-var SERVER_URL = 'http://47.111.166.154:8004/'
-var Resource_URL = 'http://47.111.166.154:8004/site/'
+var SERVER_URL = 'https://api.kangfupanda.com/'
+var Resource_URL = 'https://api.kangfupanda.com/site/'
 var $header = `<!-- !Top-bar -->
 <div id="top-bar" role="complementary" class="bit-top-bar"
     style="line-height:30px;padding-right:0px;padding-left:0px;">
