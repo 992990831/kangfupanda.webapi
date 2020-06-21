d98cf91ddebef3a24f9faca3a3edef2df53773ae
Hu, Xiaohui <xiaohuhu@stubhub.com>
2020-06-21 15:02:54 +0800

    投诉api

---
+++ kangfupanda.dataentity/DAO/ComplaintDao.cs
@@ -0,0 +1,43 @@
+﻿using kangfupanda.dataentity.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kangfupanda.dataentity.DAO
+{
+    public class ComplaintDao : BaseDao
+    {
+        public ComplaintDao(string _connStr) : base(_connStr)
+        {
+        }
+
+        public bool AddComplaint(Complaint complaint)
+        {
+            if (complaint == null)
+                return false;
+
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("insert into complaint(itemid, itemtype, title, complain, status, createdAt,updatedAt) " +
+                        "values(@itemid, @itemtype, @title, @complain, @status,now(),now())", conn);
+                    cmd.Parameters.Add(new MySqlParameter("itemid", complaint.itemId));
+                    cmd.Parameters.Add(new MySqlParameter("itemtype", complaint.itemType));
+                    cmd.Parameters.Add(new MySqlParameter("title", complaint.title));
+                    cmd.Parameters.Add(new MySqlParameter("complain", complaint.complain));
+                    cmd.Parameters.Add(new MySqlParameter("status", 0));
+                    cmd.ExecuteNonQuery();
+
+                    return true;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
---
+++ kangfupanda.dataentity/Model/Complaint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kangfupanda.dataentity.Model
+{
+    public class Complaint : BaseModel
+    {
+        public int itemId { get; set; }
+        public string itemType { get; set; }
+
+        public string title { get; set; }
+
+        public string complain { get; set; }
+
+        /// <summary>
+        /// 0未处理，1已处理
+        /// </summary>
+        public int status { get; set; }
+    }
+}
